ebcfffd9de81b731dcc21237f5b81c67046400f4
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-09-10 23:30:40 +0200

    update nextjs3

---
+++ nextjs3/pages/user-profile.js
@@ -0,0 +1,18 @@
+
+export default function UserProfilePage(props) {
+  return (
+    <h1>{props.username}</h1>
+  )
+}
+
+export async function getServerSideProps(context) {
+
+  const { params, req, res } = context
+
+
+  return {
+    props: {
+      username: 'Max'
+    }
+  }
+}
