c381957fb173e408faf69bb5062c2f4389ee9d9d
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-09-10 22:06:09 +0200

    update dynamic route

--- nextjs3/pages/index.js
+++ nextjs3/pages/index.js
@@ -1,13 +1,38 @@
+import path from 'path'
+import fs from 'fs/promises'
+import Link from 'next/link'
+
 function HomePage(props) {
+  const { products } = props 
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {
+        products.map((product)=> (
+          <Link href = {`/${product.id}`} key={product.id} ><li >{product.title}</li></Link>
+        ))
+      }
     </ul>
   );
 }
 
 
+// This function tells nextjs to pre-render this page at build time using the props returned by this function
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData);
+
+
+  
+  return {
+    props: {
+      products: data.products
+    }, // will be passed to the page component as props
+    revalidate: 10
+  }
+}
 
 export default HomePage;
+
+
