b751f03550a183d00dec2499e356114b236d6ee9
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-09-13 22:07:27 +0200

    add api_routes project file

--- Backendcode/pages/api/feedback.js
+++ Backendcode/pages/api/feedback.js
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+
 function handler (req, res){
     if ( req.method === 'POST'){
         const email = req.body.email
@@ -8,9 +11,18 @@ function handler (req, res){
             email: email,
             text: feedback
         }
+
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json')
+        const fileData = fs.readFileSync(filePath)
+        const data = JSON.parse(fileData)
+        data.push(newFeedback)
+        fs.writeFileSync(filePath, JSON.stringify(data))
+        res.status(201).json({ message: 'Success!', feedback: newFeedback})
+    } else {
+        res.status(200).json({ message: 'This works'})
     }
 
-    res.status(200).json({ message: 'This works'})
+    
 }
 
 export default handler
--- Backendcode/pages/index.js
+++ Backendcode/pages/index.js
@@ -9,6 +9,21 @@ function HomePage() {
 
     const emailData = emailRef.current.value
     const feedbackData = feedbackRef.current.value
+
+    const reqBody = {
+        email: emailData,
+        text: feedbackData
+    }
+
+    fetch('api/feedback', {
+      method: 'Post',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
   }
 
   return (
@@ -16,11 +31,11 @@ function HomePage() {
       <h1>The Home Page</h1>
       <form onSubmit={submitHandler}>
       <div>
-        <label htmlfor = 'email'>Your email adress</label>
+        <label htmlFor = 'email'>Your email adress</label>
         <input type='email' id='email' ref={emailRef}/>
       </div>
       <div>
-        <label htmlfor = 'feedback'>Tell us you thoughts</label>
+        <label htmlFor = 'feedback'>Tell us you thoughts</label>
         <textarea id = 'feedback' rows= '4' ref={feedbackRef}></textarea>
       </div>
       <button>Send feedback</button>
